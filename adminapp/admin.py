3b8c55b3888a9f6055fb35dde1868c60195462f0
RED DEVIL <58662469+onlinehacker9539@users.noreply.github.com>
2025-01-04 21:32:22 +0530

    Update admin.py

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -5,3 +5,9 @@ from adminapp.models import LoginTable, Travelagenttable
 # Register your models here.
 admin.site.register(LoginTable)
 admin.site.register(Travelagenttable)
+admin.site.register(places)
+admin.site.register(spots)
+admin.site.register(festivels)
+admin.site.register(Package_detail)
+admin.site.register(restaurant)
+admin.site.register(usertable)
